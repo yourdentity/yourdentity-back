10c847c1af09c65b2f3b2578da41ab76d290e104
HongGeonUi <keaam12@naver.com>
2025-08-11 22:33:29 +0900

    초기환경 세팅

---
+++ src/main/java/com/yourdentity/yourdentity/server/YourdentityServerApplication.java
@@ -0,0 +1,13 @@
+package com.yourdentity.yourdentity.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourdentityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourdentityServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourdentity/yourdentity/server/YourdentityServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourdentity.yourdentity.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourdentityServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
