6910c14bfbd2ef0d717088a17c91d7190a883b06
7ijin01 <dugudada01@naver.com>
2025-08-17 00:11:15 +0900

    feat: 상품 리스트, 상세정보 반환 컨트롤러

---
+++ src/main/java/com/yourdentity/yourdentity/server/store/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.yourdentity.yourdentity.server.store.controller;
+
+
+import com.yourdentity.yourdentity.server.store.dto.response.ProductListResponse;
+import com.yourdentity.yourdentity.server.store.dto.response.ProductResponse;
+import com.yourdentity.yourdentity.server.store.entity.Product;
+import com.yourdentity.yourdentity.server.store.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/store")
+@RequiredArgsConstructor
+public class ProductController
+{
+    private final ProductService productService;
+
+    @GetMapping("/products")
+    public List<ProductListResponse> getProducts()
+    {
+        return productService.findAllProducts();
+    }
+
+    @GetMapping("/product/{productId}")
+    public ProductResponse getProduct(
+            @PathVariable Long productId
+    )
+    {
+        return productService.findProductById(productId);
+    }
+}
