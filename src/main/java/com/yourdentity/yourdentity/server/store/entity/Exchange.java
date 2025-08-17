7d564521d23edb564ba6f68c70386dcac966a88a
7ijin01 <dugudada01@naver.com>
2025-08-17 23:03:47 +0900

    feat: 상품 교환 엔티티 생성

---
+++ src/main/java/com/yourdentity/yourdentity/server/store/entity/Exchange.java
@@ -0,0 +1,32 @@
+package com.yourdentity.yourdentity.server.store.entity;
+
+
+import com.yourdentity.yourdentity.global.common.base.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "product_exchange")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//TODO: 유저 엔티티 연결 필요
+public class Exchange extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Long quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private ExchangeStatus status;
+}
