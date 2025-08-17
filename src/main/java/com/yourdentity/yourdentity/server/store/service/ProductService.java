76ff4518621c098da611c83805bcda29e5fa10d3
7ijin01 <dugudada01@naver.com>
2025-08-17 15:07:41 +0900

    feat: 리뷰/질문 조회 로직 분리 및 product에 세팅

--- src/main/java/com/yourdentity/yourdentity/server/store/service/ProductService.java
+++ src/main/java/com/yourdentity/yourdentity/server/store/service/ProductService.java
@@ -3,6 +3,9 @@ package com.yourdentity.yourdentity.server.store.service;
 import com.yourdentity.yourdentity.server.store.dto.response.ProductListResponse;
 import com.yourdentity.yourdentity.server.store.dto.response.ProductResponse;
 import com.yourdentity.yourdentity.server.store.entity.Product;
+import com.yourdentity.yourdentity.server.store.entity.ProductOption;
+import com.yourdentity.yourdentity.server.store.entity.ProductQuestion;
+import com.yourdentity.yourdentity.server.store.entity.ProductReview;
 import com.yourdentity.yourdentity.server.store.mapper.ProductDetailMapper;
 import com.yourdentity.yourdentity.server.store.mapper.ProductListMapper;
 import com.yourdentity.yourdentity.server.store.repository.ProductRepository;
@@ -23,15 +26,21 @@ public class ProductService
 
     public List<ProductListResponse> findAllProducts()
     {
-        List<Product> products = productRepository.findAllWithOptionsAndDescriptionsAndTags()
-                .orElseThrow(() -> new EntityNotFoundException("Product not found"));
+        List<Product> products = productRepository.findAllWithFetch();
         return productListMapper.toListResponses(products);
     }
 
     public ProductResponse findProductById(long productId)
     {
-        Product product= productRepository.findByIdWithOptionsAndDescriptionsAndTags(productId)
+        Product product = productRepository.findByIdWithFetch(productId)
                 .orElseThrow(() -> new EntityNotFoundException("Product not found"));
+
+        List<ProductQuestion> questions = productRepository.findQuestionsByProductId(productId);
+        List<ProductReview> reviews = productRepository.findReviewsByProductId(productId);
+        product.setQuestions(questions);
+        product.setReviews(reviews);
+
+
         return productDetailMapper.toResponse(product);
     }
 }
