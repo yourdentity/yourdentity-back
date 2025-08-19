e0e37fde0804128cb369c93b893ec59c511ef088
7ijin01 <dugudada01@naver.com>
2025-08-19 19:25:30 +0900

    fix: Exchange -> Order 엔티티로 변경

--- src/main/java/com/yourdentity/yourdentity/server/store/dto/request/OrderCheckRequest.java
+++ src/main/java/com/yourdentity/yourdentity/server/store/dto/request/OrderCheckRequest.java
@@ -2,8 +2,8 @@ package com.yourdentity.yourdentity.server.store.dto.request;
 
 import java.util.List;
 
-public record ExchangeRequest(
-        List<ExchangeItemRequest> items
+public record OrderCheckRequest(
+        List<OrderItemRequest> items
 )
 {
 }
